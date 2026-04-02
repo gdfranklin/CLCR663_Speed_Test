8f2deffc8a507f328517a1891dc2de06404eb7e8
gdfranklin <gdfranklin@shaw.ca>
2026-04-02 12:25:35 -0700

    update comments

--- CLCR663_Speed_Test/CLCR663_Speed_Test.ino
+++ CLCR663_Speed_Test/CLCR663_Speed_Test.ino
@@ -44,38 +44,38 @@ void setup() {
 
   Serial.println("Ready");
 }
-
-void loop(){
-  static int readCount = 0;           // number of consecutive valid reads
-  static int sequence = 1;            // tag seen count
+  
+int sequence = 1;            // Count complete sequences of reads
+  static int goodReadCount = 0;       // number good reads in this sequence
+  static int badReadCount = 0;        // number of bad reads in this sequence
+  static int noTagCount = 0;          // number of no tag reads since the last good read
   static unsigned long firstSeenTime; // start time of a series of good reads   
   uint8_t uid[10]={0};                // variable for 10byte UID
   uint8_t uid_len;                    // length of returned UID
-  static int noTagCount = 0;          // number of no tag reads since the last good read
-  static int totalNoTagCount = 0;     // number of no tags ingnored since the first good read
 
+void loop(){
   unsigned long commandStartTime = millis();
   uid_len = reader.read_iso18693_uid(uid); // tag read function
   unsigned long ElapsedTime = (millis() - commandStartTime);  
 
   if (uid_len != 0) {                 // Successful read?
     noTagCount = 0;                   // reset the no tag count
-    readCount++;                      
-    if (readCount==1) {               // first successful read?
-      firstSeenTime = millis();
-      totalNoTagCount = 0;            // reset total no tag count 
-      static char UID[50];    // display the tag info      
+    goodReadCount++;                  // increment the number of good reads
+    if (goodReadCount==1) {           // Is this the start of a new sequence?
+      firstSeenTime = millis();       // Mark the start of a new sequence
+      badReadCount = 0;               // reset bad reads at the start of a new sequence
+      static char UID[50];            // display the tag info      
       sprintf(UID, "%02X %02X %02X %02X %02X %02X %02X %02X", uid[7],uid[6],uid[5],uid[4],uid[3],uid[2],uid[1],uid[0]);
       Serial.printf("Sequence: %d, UID: %s, Read Time: %dms\n", sequence++, UID, ElapsedTime);
     }
-   } else {                           // No tag in range
-    noTagCount++;
-    totalNoTagCount++;
-    if (readCount>0) {                // have we seen a tag
-      if (noTagCount > 4) {           // wait for a few few noTag returns
+   } else {                           // No tag was read
+    noTagCount++;                     // Increment the number times we have not seen a tag in a row
+    badReadCount++;                   // increment the number of time we have not seen a tag in this sequence
+    if (goodReadCount>0) {            // Are we in a sequence?
+     if (noTagCount >= 3) {           // If we have not seen tags in 3 reads then we are at the end of a sequence 
         unsigned long timeSeen = (millis() - firstSeenTime);    
-        Serial.printf("Total Time Seen: %d mSec. reads: %d, No Tag Count: %d\n\n", timeSeen, readCount, totalNoTagCount);                 
-        readCount = 0;                  // reset
+        Serial.printf("Total Time Seen: %d mSec. Good Reads: %d, Bad Reads: %d\n\n", timeSeen, goodReadCount, badReadCount-3);                 
+        goodReadCount = 0;            // Reset readcount indicating the sequence has ended
       }
     }
   } 
